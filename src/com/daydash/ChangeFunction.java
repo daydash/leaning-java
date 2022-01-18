9141d8dc365e4e27c7e3c5b7a359c2f0698c7457
daydash <thedaydash@gmail.com>
2022-01-19 01:37:36 +0530

    Modified ChangeFunction.java

--- src/com/daydash/ChangeFunction.java
+++ src/com/daydash/ChangeFunction.java
@@ -7,6 +7,7 @@ public class ChangeFunction{
         public static void main(String[] args) {
             // create an array
             int[] arr = {1, 3, 2, 45, 6};
+            System.out.println(Arrays.toString(arr));
             change(arr);
             System.out.println(Arrays.toString(arr));
         }
