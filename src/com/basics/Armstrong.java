c22d68f2a2feca24acc4d960fd5f80395d269499
daydash <thedaydash@gmail.com>
2022-01-19 21:44:46 +0530

    added new programs

---
+++ src/com/basics/Armstrong.java
@@ -0,0 +1,36 @@
+package com.basics;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number to check : ");
+        int n = in.nextInt();
+        int temp = n;
+        int checker = n;
+        int count = 0, ans = 0;
+
+        // Counting the length of number
+        while (n>0){
+            n/=10;
+            count++;
+        }
+
+        // Calculation the answer
+        while (temp>0){
+            ans = ans + (int)Math.pow(temp%10,count);
+            temp/=10;
+        }
+
+        // Checking the printing if the number is armstrong
+        if (checker == ans){
+            System.out.println(checker + " is Armstrong");
+        }
+        else{
+            System.out.println(checker + " is not Armstrong");
+        }
+
+    }
+}
---
+++ src/com/basics/Palindrime.java
@@ -0,0 +1,26 @@
+package com.basics;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Palindrime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the string : ");
+        String str = in.next();
+
+        /* this program is case-sensitive
+        to make is independent of it use the command as follows */
+        // str = str.toLowerCase();
+
+        //checking whether the str is 'NOT' palindrome
+        for (int i = 0; i < str.length()/2 ; i++){
+            if(str.charAt(i) != str.charAt(str.length()-i-1)){
+                System.out.println(str + " is NOT palindrome");
+                return;
+            }
+        }
+
+        System.out.println(str + " is Palindrome");
+    }
+}
