15fe462f1d986315c98c2c283cf149775bd2f1c3
daydash <thedaydash@gmail.com>
2022-01-20 04:02:14 +0530

    added basic stuff

--- src/com/basics/Main.java
+++ src/com/basics/Main.java
@@ -3,5 +3,38 @@ package com.basics;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hey Yash");
+
+//        use of modulus operator %
+
+//        smaller % greater = smaller
+//        equal % equal = 0
+
+        System.out.println(9 % 5);          // 4
+        System.out.println(9.0 % 5);        // 4.0
+        System.out.println(9.1 % 5);        // 4.1
+        System.out.println(9.1 % 5.1);      // 4.0
+        System.out.println(9.1 % 5.3);      // 3.8
+        System.out.println(5 % 5);          // 0
+        System.out.println(-15 % 2);        // -1
+        System.out.println(15 % -2);        // 1
+        System.out.println(0.3 % 0.5);      // 0.3
+        System.out.println(0.5 % 0.3);      // 0.2
+
+        char ch1 = 'Y';
+        char ch2 = 88; // ASCII value of X
+        System.out.println(ch1 + " " + ch2); //X Y
+
+        System.out.println('a' + 1);        // 98       - ASCII value of a is 97, so 97+1=98
+        System.out.println('a' + '1');      // 146      - ASCII value of a is 97 & '1' is 49, so 97+49=146
+        System.out.println("a + 1");        // a + 1    - string
+
+        // Specify integer using its binary code with prefix as 0b or 0B, 1010(in binary) = 10(in decimal)
+        int x = 0B1010;
+        System.out.println("x = " + x);
+
+        // Specify integer using its hexadecimal code with prefix as 0x or 0X, A(in hexadecimal) = 10(in decimal)
+        int y = 0xA;
+        System.out.println("y = " + y);
+
     }
 }
