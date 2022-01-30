a19581cdb9d812c2a747078099dc0819e8e197bf
daydash <thedaydash@gmail.com>
2022-01-31 01:26:16 +0530

    updated AddToArrayFormOfInteger

--- src/com/arrays/AddToArrayFormOfInteger.java
+++ src/com/arrays/AddToArrayFormOfInteger.java
@@ -1,6 +1,7 @@
 package com.arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class AddToArrayFormOfInteger {
@@ -9,30 +10,34 @@ public class AddToArrayFormOfInteger {
         int[] nums = {1, 2, 0, 0};
         int[] nums2 = {2, 7, 4};
         int[] nums3 = {2, 1, 5};
+        int[] nums4 = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
 
         int k = 34;
         int k2 = 181;
         int k3 = 806;
+        int k4 = 1;
 
         ArrayList<Integer> ans = addToArrayForm(nums, k);
         ArrayList<Integer> ans2 = addToArrayForm(nums2, k2);
         ArrayList<Integer> ans3 = addToArrayForm(nums3, k3);
+        ArrayList<Integer> ans4 = addToArrayForm(nums4, k4);
 
         System.out.println(ans);
         System.out.println(ans2);
         System.out.println(ans3);
+        System.out.println(ans4);
 
     }
 
     static ArrayList<Integer> addToArrayForm(int[] num, int k) {
         ArrayList<Integer> ans = new ArrayList<>(num.length);
-        int number = 0;
+        long number = 0; // int -> long
         for (int i = 0; i < num.length; i++) {
             number += num[i] * Math.pow(10, num.length - 1 - i);
         }
-        int sum = number + k;
+        long sum = number + k; // int -> long
         while (sum > 0) {
-            int digit = sum % 10;
+            int digit = (int)(sum % 10); // cast to int
 //            ans.add(num.length-1,digit); // (IGNORE) logic is partially-correct but the array is empty... like {null,null,null,null, digit} // also the index will remain same ans will not get update for the next value...
             ans.add(digit);
             sum /= 10;
