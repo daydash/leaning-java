3801f32d95c1ac15a49d3e8617f2a63824380a45
daydash <thedaydash@gmail.com>
2022-01-31 00:11:33 +0530

    added FindNumbersWithEvenNumberOfDigits

---
+++ src/com/arrays/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,32 @@
+package com.arrays;
+
+public class FindNumbersWithEvenNumberOfDigits {
+    public static void main(String[] args) {
+
+        int[] nums = {12, 345, 2, 6, 7896};
+        int[] nums2 = {555, 901, 482, 1771};
+
+        int ans = findNumbers(nums);
+        int ans2 = findNumbers(nums2);
+
+        System.out.println(ans);
+        System.out.println(ans2);
+    }
+
+    static int findNumbers(int[] nums) {
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (isEvenDigits(nums[i])) cnt++;
+        }
+        return cnt;
+    }
+
+    static boolean isEvenDigits(int n) {
+        int cnt = 0; // local variable... doesn't affect the cnt in findNumbers()
+        while (n > 0) {
+            n /= 10;
+            cnt++;
+        }
+        return (cnt & 1) == 0; // provides a value either true or false because of relational operator, i.e. ==
+    }
+}
--- src/com/arrays/FindTheHighestAltitude.java
+++ src/com/arrays/FindTheHighestAltitude.java
@@ -9,17 +9,8 @@ link - https://leetcode.com/problems/find-the-highest-altitude/
 
 package com.arrays;
 
-import java.util.Arrays;
-
 public class FindTheHighestAltitude {
     public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
-
-//        int[] nums = new int[6];
-//
-//        for (int i = 0; i < nums.length; i++) {
-//            nums[i] = in.nextInt();
-//        }
 
         int[] nums = {-5,1,5,0,-7};
         int[] nums2 = {-4,-3,-2,-1,4,3,2};
