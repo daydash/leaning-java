49cb1a9e482542918ed368f2d07a2d4db185352f
daydash <thedaydash@gmail.com>
2022-02-08 04:47:41 +0530

    added strings

---
+++ src/com/strings/Main.java
@@ -0,0 +1,20 @@
+package com.strings;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Yash Bhatnagar";
+        System.out.println(name);
+
+        String a = "Yash";
+        System.out.println(a);
+        a = "Bhatnagar";       // Not modified the object as wkt string are immutable, instead creating a new object in the pool
+        System.out.println(a);
+
+        // the questions arise here is that...
+        // where does the original object "Yash" goes when "Bhatnagar" is made and a(ref. variable) is now pointing toward "Bhatnagar" meanwhile "Yash" has no reference variable???
+        // The very cool and short answer to is: Garbage Collector #RIPCpp #JavaVsC++
+    }
+}
