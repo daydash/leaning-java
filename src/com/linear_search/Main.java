9046a4e3a0c1b7ff8ac269c6c9e2f109bb60ecd1
daydash <thedaydash@gmail.com>
2022-02-13 22:26:12 +0530

    added boolean and index fuction of linear search

--- src/com/linear_search/Main.java
+++ src/com/linear_search/Main.java
@@ -25,18 +25,53 @@ public class Main {
 
         int target = in.nextInt();
 
-        int ans = linearSearch2(list, target);
-        System.out.println(ans);
+        int ans = linearSearch4(list, target);
+        System.out.println("Element found at index: " + ans);
 
     }
 
-    static int linearSearch2(ArrayList<Integer> list, int target) {
+    static int linearSearch4(ArrayList<Integer> list, int target) {
         if (list.isEmpty()) return -1;
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i) == target) return i;
         }
         return -1;
     }
+
+    // search the target and return true or false
+    static boolean linearSearch3(int[] arr, int target) {
+        if (arr.length == 0) {
+            return false;
+        }
+
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return false;
+    }
+
+    // search the target and return the element
+    static int linearSearch2(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return element;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return Integer.MAX_VALUE;
+    }
+
 
     // search in the array: return the index if item found
     // otherwise if item not found return -1
